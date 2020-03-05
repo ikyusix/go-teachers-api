22c0b111aef4e4f02d461cea8f6efa029db755cc
bima adam nugraha <bimaadamnugraha@gmail.com>
2020-03-05 13:12:05 +0700

    [IMPROVE] Connection to database

--- models/teacher_model.go
+++ models/teacher_model.go
@@ -1,1 +1,10 @@
 package models
+
+import "github.com/jinzhu/gorm"
+
+type Teachers struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
